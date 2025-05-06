bb5596554d1b3ce9f45ca00374f8cc1a296af52a
vartika <dhochakvartika@gmail.com>
2025-05-06 10:25:52 +0530

    Add Reverse Linked List Java code and update README

---
+++ ReverseLL.java
@@ -0,0 +1,56 @@
+// ReverseLL.java
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class ReverseLL {
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next; // save next node
+            current.next = prev;              // reverse the link
+            prev = current;                   // move prev forward
+            current = nextNode;               // move current forward
+        }
+
+        return prev; // new head
+    }
+
+    // Helper method to print the list
+    public void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ReverseLL solution = new ReverseLL();
+
+        // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original list:");
+        solution.printList(head);
+
+        // Reverse the list
+        ListNode reversedHead = solution.reverseList(head);
+
+        System.out.println("Reversed list:");
+        solution.printList(reversedHead);
+    }
+}
