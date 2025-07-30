655c6ea9571de20c14f276e9d997498a659334f8
vartikadhochak <dhochakvartika@gmail.com>
2025-07-31 00:55:21 +0530

    Time: 2 ms (96.67%) | Memory: 41.8 MB (65.88%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,29 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        // Create a stack to store the opening brackets
         Stack<Character> stack = new Stack<>();
         
-        // Iterate through each character in the string
         for (char c : s.toCharArray()) {
-            // If the character is an opening bracket, push it onto the stack
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             }
-            // If the character is a closing bracket
             else {
-                // If the stack is empty or the top of the stack doesn't match the current closing bracket
                 if (stack.isEmpty()) return false;
                 
                 char top = stack.pop();
-                
-                // Check if the current closing bracket matches the last opened bracket
                 if (c == ')' && top != '(') return false;
                 if (c == '}' && top != '{') return false;
                 if (c == ']' && top != '[') return false;
             }
         }
-        
-        // If the stack is empty, all brackets were correctly closed
         return stack.isEmpty();
     }
 }
