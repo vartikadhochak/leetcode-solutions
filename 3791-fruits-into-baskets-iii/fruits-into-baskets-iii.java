aee8c90cb001e5c8c86a2e1faf26f6b30e590e14
vartikadhochak <dhochakvartika@gmail.com>
2025-08-06 20:48:18 +0530

    Time: 88 ms (48.65%) | Memory: 65 MB (46.85%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.java
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.java
@@ -1,9 +1,11 @@
 class Solution {
     int n;
     int[] seg;
+
     void Update(int p) { 
         seg[p] = Math.max(seg[p << 1], seg[p << 1 | 1]); 
     }
+
     void Build(int p, int l, int r, int[] baskets) {
         if (l == r) {
             seg[p] = baskets[l];
@@ -14,6 +16,7 @@ class Solution {
         Build(p << 1 | 1, mid + 1, r, baskets);
         Update(p);
     }
+
     void Assign(int x, int v, int p, int l, int r) {
         if (x < l || x > r) {
             return;
@@ -27,12 +30,13 @@ class Solution {
         Assign(x, v, p << 1 | 1, mid + 1, r);
         Update(p);
     }
+
     int FirstLarger(int v, int p, int l, int r) {
         if (seg[p] < v) {
             return r + 1;
         }
         if (l == r) {
-            return r;
+            return l;
         }
         int mid = (l + r) >> 1;
         int lf = FirstLarger(v, p << 1, l, mid);
@@ -41,6 +45,7 @@ class Solution {
         }
         return FirstLarger(v, p << 1 | 1, mid + 1, r);
     }
+
     public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
         n = fruits.length;
         seg = new int[4 * n + 1];
